75c8a153a890a894bccd1271445c56cd06c3c4ef
alinous-sys <fliperspam@gmail.com>
2025-06-07 22:39:40 +0200

    test

--- budgetlog/wsgi.py
+++ budgetlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for budget_log project.
+WSGI config for budgetlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budget_log.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetlog.settings')
 
 application = get_wsgi_application()
